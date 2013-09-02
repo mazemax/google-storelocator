430f8ec9eddb472d01f9960ef4a3933c8a6f083c
M.Saad Siddiqui <to[dot]msaads[at-the-rate]gmail[dot]com>
2013-09-03 03:33:53 +0500

    Version2

---
+++ js/global_store.js
@@ -0,0 +1,24 @@
+google.maps.event.addDomListener(window, 'load', function() 
+{
+  var map = new google.maps.Map(document.getElementById('map-canvas'), 
+  {
+    center: new google.maps.LatLng(-28, 135),
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var panelDiv = document.getElementById('panel');
+
+  var data = new MedicareDataSource;
+
+  var view = new storeLocator.View(map, data, 
+  {
+    geolocation: true,
+    features: data.getFeatures()
+  });
+
+  new storeLocator.Panel(panelDiv, 
+  {
+    view: view
+  });
+});
